34aebc34fe4613ce7a2827cfca376678d6e14c1f
troy213 <tritera.erlangga@gmail.com>
2023-02-05 05:54:05 +0700

    💄 Styling not found page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -3,10 +3,12 @@ import { PokeballSolid } from '../../assets/images'
 const NotFound = () => {
   return (
     <section className='not-found flex-column flex-justify-center flex-align-center text-color-light gap-4'>
-      <p className='flex flex-align-center gap-4 text-8 text-bold'>
-        4<PokeballSolid />4
-      </p>
-      <p className=''>Sorry, the page is not exist :(</p>
+      <div className='not-found__font flex flex-align-center gap-4 text-8 text-bold'>
+        <span>4</span>
+        <PokeballSolid className='not-found__pokeball' />
+        <span>4</span>
+      </div>
+      <p className='text-light'>Sorry, the page is not exist :(</p>
     </section>
   )
 }
